fd8d92651fe065163a565abb1fdffe3e99716141
Grecia Macias <greciam@umich.edu>
2025-07-09 14:49:19 -0400

    Add exercise requirements

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+// Purpose: The purpose of this exercise is to practice Test Driven Development
+// Subject: A site that has movies and allows you to search different movies
+// Requirement 1: Search movie by partial title
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
